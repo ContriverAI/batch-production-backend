c8adf4f54dd794d56e0ae6c9788fd9685cfa3712
bramhesh <bramhesh.srivastav@algo8.ai>
2020-09-18 15:12:19 +0530

    time issue resolved

--- logical_data_update.py
+++ logical_data_update.py
@@ -23,17 +23,24 @@ def cooling_update():
             t1 = str(t1)
             t1 = t1.split()
             t1 = t1[2]
+            print("--time old--------{}----------".format(t1))
             crnttime = time.strftime('%H:%M:%S')
             crnttime = str(crnttime)
             crnttime = crnttime.split()
             crnttime = crnttime[0]
+            print("--current time--------{}----------".format(crnttime))
             FMT = '%H:%M:%S'
             tdelta = datetime.strptime(t1, FMT) - datetime.strptime(crnttime, FMT)
-            if tdelta.seconds == 0 and tdelta.seconds > 60:
+            print("--tdelta--------{}----------".format(tdelta))
+            print("--tdelta seconds--------{}----------".format(tdelta.seconds))
+            if tdelta.seconds == 0 or tdelta.seconds > 20060:
                 newtime = "00:00:00"
+                print("--newtime--------{}----------".format(newtime))
             else:
                 tdelts = timedelta(days=0,seconds=tdelta.seconds, microseconds=tdelta.microseconds)
                 newtime = str(tdelts)
+                print("--newtime--------{}----------".format(newtime))
+            query = "update Cooling set `remaining time` = '"+str(newtime)+"' where trolley = "+str(tr)+";"
+            print("--query--{}".format(query))
             x = threading.Thread(target=df_bg_cooling, args=(tr,newtime,))
             x.start()
-#        print("cooling updated with complete time")
