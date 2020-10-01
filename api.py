3ae65841add1b498cecb79bc851c9435cf9f8878
bramhesh <bramhesh.srivastav@algo8.ai>
2020-10-02 00:09:52 +0530

    time converted

--- api.py
+++ api.py
@@ -44,22 +44,43 @@ def softner_v2(l1):
 #         print("PM rem not done")
 #     return t1
 
-def softner_t1(str1):
-    print("----------{}-----".format(str1))
+# def softner_t1(str1):
+#     print("----------{}-----".format(str1))
+#     if str1[-2:] == "AM" and str1[:2] == "12": 
+#         return "00" + str1[2:-2] 
+          
+#     # remove the AM     
+#     elif str1[-2:] == "AM": 
+#         return str1[:-2] 
+      
+#     # Checking if last two elements of time 
+#     # is PM and first two elements are 12    
+#     elif str1[-2:] == "PM" and str1[:2] == "12": 
+#         return str1[:-2] 
+          
+#     else:
+#         return str(int(str1[:2]) + 12) + str1[2:8] 
+
+def softner_t1(str1): 
+      
+    # Checking if last two elements of time 
+    # is AM and first two elements are 12 
+    if len(str1) <=10 :
+        str1 = "0" + str1
+        
     if str1[-2:] == "AM" and str1[:2] == "12": 
         return "00" + str1[2:-2] 
-          
     # remove the AM     
     elif str1[-2:] == "AM": 
-        return str1[:-2] 
+        return str1[:-2]
       
     # Checking if last two elements of time 
     # is PM and first two elements are 12    
     elif str1[-2:] == "PM" and str1[:2] == "12": 
         return str1[:-2] 
-          
-    else:
-        return str(int(str1[:2]) + 12) + str1[2:8] 
+    else: 
+        # add 12 to hours and remove PM 
+        return str(int(str1[:2]) + 12) + str1[2:8]
 
 def getcoolingdata(socketio):
     coolingdata = data_storage.cooling()
--- testapi.py
+++ testapi.py
@@ -1,17 +1,18 @@
-from flask import Flask, url_for, request, render_template, jsonify
-import threading
-import pandas as pd
-import data_storage
-from flask_cors import CORS
-import json
+def softner_t1(str1):
+    print("----------{}-----".format(str1))
+    if str1[-2:] == "AM" and str1[:2] == "12": 
+        return "00" + str1[2:-2] 
+          
+    # remove the AM     
+    elif str1[-2:] == "AM": 
+        return str1[:-2] 
+      
+    # Checking if last two elements of time 
+    # is PM and first two elements are 12    
+    elif str1[-2:] == "PM" and str1[:2] == "12": 
+        return str1[:-2] 
+          
+    else:
+        return str(int(str1[:1]) + 12) + str1[2:8]
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/get/users', methods = ['GET', 'POST'])
-def getusers():
-    data = request.json
-    print(data['username'])
-    return 'done'
-
-app.run(host='0.0.0.0', port=9001)
+print(softner_t1("01:05 PM"))
